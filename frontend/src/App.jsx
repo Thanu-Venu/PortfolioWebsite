124bf6a5643beadfec1c86110b481e748b5e9a9b
Thanu Venu <thanu.venu28@gmail.com>
2026-03-28 09:10:09 +0530

    Added all the react and tailwind related files created basic folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <h1 className="text-center text-3xl mt-10">Hello Portfolio 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
